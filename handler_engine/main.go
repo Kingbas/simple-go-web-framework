5d3e8fb23991bb386b9d3698d1f74942002c63e3
Kingbas <1044767717@qq.com>
2023-03-15 01:30:26 +0800

    use golang workspace and init gee

---
+++ handler_engine/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Engine is the uni handler for all requests
+type Engine struct{}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	case "/ping":
+		log.Print("someone pinged")
+		fmt.Fprintf(w, "pong")
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
+
+func main() {
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":8080", engine))
+}
